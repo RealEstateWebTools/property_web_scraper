5b45e080c68ea79849b5a69068625bfc25034096
Ed Tewiah <etewiah@hotmail.cim>
2017-06-19 14:26:52 +0100

    Add scraper service

--- app/controllers/property_web_scraper/scraper_controller.rb
+++ app/controllers/property_web_scraper/scraper_controller.rb
@@ -3,9 +3,21 @@ require_dependency "property_web_scraper/application_controller"
 module PropertyWebScraper
   class ScraperController < ApplicationController
     def retrieve
-      render json: {
-        success: true
-      }
+
+      unless params[:source_url].present?
+        return render json: { error: "Please provide url."}, status: 422
+      end
+
+      target_url = params[:source_url]
+
+      retrieved_properties = PropertyWebScraper::Scraper.new(target_url).retrieve_from_webpage
+
+      render json: retrieved_properties
+
+
+      # render json: {
+      #   success: true
+      # }
     end
   end
 end
