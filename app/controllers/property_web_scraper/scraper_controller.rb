7c8cc0f872b560db02fa0c8167bc619c99678b00
Ed Tewiah <etewiah@hotmail.cim>
2017-08-24 16:59:23 +0200

    Added retrieve_as_json endpoint

--- app/controllers/property_web_scraper/scraper_controller.rb
+++ app/controllers/property_web_scraper/scraper_controller.rb
@@ -2,6 +2,62 @@ require_dependency 'property_web_scraper/application_controller'
 
 module PropertyWebScraper
   class ScraperController < ApplicationController
+    # below to avoid ActionController::InvalidAuthenticityToken error when posting from chrome extension
+    protect_from_forgery with: :null_session
+    # def show
+    #   import_host = PropertyWebScraper::ImportHost.find_by(id: params[:id])
+
+    #   import_host_listings = PropertyWebScraper::Listing.where(import_host_id: import_host.id)
+    # end
+
+    # def retrieve
+    #   unless params[:import_url].present?
+    #     return render json: { error: 'Please provide url.' }, status: 422
+    #   end
+    #   import_url = params[:import_url].strip
+    #   import_host = PropertyWebScraper::ImportHost.find_by(id: params[:id])
+
+    #   web_scraper = PropertyWebScraper::Scraper.new(import_host.scraper_name)
+    #   listing = web_scraper.process_url import_url, import_host
+
+    #   render json: listing.as_json
+
+    #   # render json: {
+    #   #   success: true
+    #   # }
+    # end
+
+    def welcome
+      # @scraper_configs_coll = PropertyWebScraper::ImportHost.all
+    end
+
+    def retrieve_as_json
+
+      unless params["url"]
+        return render json: {
+          success: false,
+          error_message: "Please provide a url"
+        }
+      end
+      uri = uri_from_url params["url"].strip
+      unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+        return render json: {
+          success: false,
+          error_message: "Please provide a valid url"
+        }
+      end
+      import_host = PropertyWebScraper::ImportHost.find_by_host(uri.host)
+      unless import_host
+        return render json: {
+          success: false,
+          error_message: "Sorry, the url provided is currently not supported"
+        }
+      end
+      web_scraper = PropertyWebScraper::Scraper.new(import_host.scraper_name)
+      @listing = web_scraper.process_url uri.to_s, import_host
+      render json: @listing.to_json
+    end
+
     def ajax_submit
       # scraper_name = params[:scraper][:scraper_name]
       import_url = params[:import_url].strip
@@ -12,6 +68,7 @@ module PropertyWebScraper
       end
       @listing = {}
       if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+        # find import_host with meta data from db
         import_host = PropertyWebScraper::ImportHost.find_by_host(uri.host)
         if import_host
           @success = true
@@ -48,31 +105,15 @@ module PropertyWebScraper
       render '/property_web_scraper/retrieve_results.js.erb', layout: false
     end
 
-    def show
-      import_host = PropertyWebScraper::ImportHost.find_by(id: params[:id])
+    private
 
-      import_host_listings = PropertyWebScraper::Listing.where(import_host_id: import_host.id)
-    end
-
-    def welcome
-      @scraper_configs_coll = PropertyWebScraper::ImportHost.all
-    end
-
-    def retrieve
-      unless params[:import_url].present?
-        return render json: { error: 'Please provide url.' }, status: 422
+    def uri_from_url import_url
+      begin
+        uri = URI.parse import_url
+      rescue URI::InvalidURIError => error
+        uri = ""
       end
-      import_url = params[:import_url].strip
-      import_host = PropertyWebScraper::ImportHost.find_by(id: params[:id])
-
-      web_scraper = PropertyWebScraper::Scraper.new(import_host.scraper_name)
-      listing = web_scraper.process_url import_url, import_host
-
-      render json: listing.as_json
-
-      # render json: {
-      #   success: true
-      # }
     end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 PropertyWebScraper::Engine.routes.draw do
   root to: 'scraper#welcome'
-  get '/retrieve' => 'scraper#retrieve'
-  get '/scrapers/:id' => 'scraper#show'
-  get '/scrapers/:id/retrieve' => 'scraper#retrieve'
+  # get '/retrieve' => 'scraper#retrieve'
+  # get '/scrapers/:id' => 'scraper#show'
+  # get '/scrapers/:id/retrieve' => 'scraper#retrieve'
   post '/scrapers/submit' => 'scraper#ajax_submit'
+  post '/retriever/as_json' => 'scraper#retrieve_as_json'
+  get '/retriever/as_json' => 'scraper#retrieve_as_json'
 end
