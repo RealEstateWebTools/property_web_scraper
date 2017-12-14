e167b983a9fe67df06de34adf8bc190fc905b710
Ed Tewiah <etewiah@hotmail.cim>
2017-12-14 17:40:14 +0100

    Add and use vue-google-maps component

--- app/assets/javascripts/property_web_scraper/vue/vue-main.js
+++ app/assets/javascripts/property_web_scraper/vue/vue-main.js
@@ -5,13 +5,13 @@ window.onload = function() {
 
   var pwbSS = Vue.component('social-sharing', SocialSharing);
   // // var pwbGM = Vue.component('gmap-map', VueGoogleMaps);
-  // Vue.use(VueGoogleMaps, {
-  //   load: {
-  //     key: 'AIzaSyCPorm8YzIaUGhKfe5cvpgofZ_gdT8hdZw'
-  //     // v: '3.26', // Google Maps API version
-  //     // libraries: 'places',   // If you want to use places input
-  //   }
-  // });
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyCPorm8YzIaUGhKfe5cvpgofZ_gdT8hdZw'
+      // v: '3.26', // Google Maps API version
+      // libraries: 'places',   // If you want to use places input
+    }
+  });
   var markers = PWSAPP.markers || [];
   PWSAPP.pwbVue = new Vue({
     el: '#main-vue',
