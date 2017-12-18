35daa6d569ee7f8e9b29b13e0e9f46a13a63ddf3
Ed Tewiah <etewiah@hotmail.cim>
2017-12-18 22:52:08 +0100

    Set up asset precompilation

--- lib/property_web_scraper/engine.rb
+++ lib/property_web_scraper/engine.rb
@@ -15,6 +15,14 @@ module PropertyWebScraper
     #   app.middleware.use MyEngine::Middleware
     # end
 
+    initializer "property_web_scraper.assets.precompile" do |app|
+      app.config.assets.precompile += %w( property_web_scraper/spp_vuetify.css property_web_scraper/spp_vuetify.js )
+    end
+    # seems above is the preferred way of setting up precompilation in an engine
+    # http://edgeguides.rubyonrails.org/engines.html#separate-assets-precompiling
+    # config.assets.precompile += %w( property_web_scraper/spp_vuetify.css property_web_scraper/spp_vuetify.js )
+
+
     config.generators do |g|
       g.test_framework :rspec, fixture: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
